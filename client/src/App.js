bdc073e3998c9d0bb0c1d9caeb6d168e763d7005
Michael <checomichael2@gmail.com>
2019-02-26 17:28:32 -0500

    converted App to use hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ApolloProvider } from 'react-apollo-hooks';
 import './App.css';
 import apolloClient from './apolloClient';
 import TokenForm from './TokenForm';
 import StarredRepos from './StarredRepos';
 
-class App extends Component {
-	state = {
-		token: null,
-	};
-	componentDidMount() {
-		this.setState({ token: sessionStorage.getItem('token') });
-	}
-	setToken = token => {
+function App() {
+	const [token, setToken] = useState(null);
+
+	useEffect(() => {
+		setToken({ token: sessionStorage.getItem('token') });
+	}, [token]);
+	const setAToken = token => {
 		sessionStorage.setItem('token', token);
 		this.setState({ token });
 	};
-	render() {
-		const { token } = this.state;
-		return (
-			<div className="App">
-				<ApolloProvider client={apolloClient}>
-					{token ? <StarredRepos /> : <TokenForm setToken={this.setToken} />}
-				</ApolloProvider>
-			</div>
-		);
-	}
+
+	return (
+		<div className="App">
+			<ApolloProvider client={apolloClient}>
+				{token ? <StarredRepos /> : <TokenForm setToken={setAToken} />}
+			</ApolloProvider>
+		</div>
+	);
 }
+// class App extends Component {
+// 	state = {
+// 		token: null,
+// 	};
+// 	componentDidMount() {
+// 		this.setState({ token: sessionStorage.getItem('token') });
+// 	}
+// 	setToken = token => {
+// 		sessionStorage.setItem('token', token);
+// 		this.setState({ token });
+// 	};
+// 	render() {
+// 		const { token } = this.state;
+// 		return (
+// 			<div className="App">
+// 				<ApolloProvider client={apolloClient}>
+// 					{token ? <StarredRepos /> : <TokenForm setToken={this.setToken} />}
+// 				</ApolloProvider>
+// 			</div>
+// 		);
+// 	}
+// }
 
 export default App;
